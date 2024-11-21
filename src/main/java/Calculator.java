5ef0e4f3ca6acee4a7908e865665642f848d2149
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2024-11-22 01:50:58 +0330

    implement multiplication and add test scenarios

--- src/main/java/Calculator.java
+++
@@ -1,6 +0,0 @@
-public class Calculator {
-
-    public double calculate() {
-        return 0;
-    }
-}
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+
+    public double calculate(double firstNumber, double secondNumber, char operator) {
+        if (operator == '*') {
+            return firstNumber * secondNumber;
+        }
+        return 0;
+    }
+}
